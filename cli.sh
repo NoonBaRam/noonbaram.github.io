e558b306233b05344802bcaba2d2203e3e955726
NoonBaRam <132915445+NoonBaRam@users.noreply.github.com>
2024-12-02 23:38:54 +0900

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -6,6 +6,7 @@ tar -zxf $HOME/cli/eksctl_Linux_amd64.tar.gz -C $HOME/cli/
 sudo cp $HOME/cli/eksctl /usr/local/bin
 eksctl completion bash >> $HOME/.bashrc-completion
 source $HOME/.bashrc-completion
+echo '~!~!~!~!~!~! eksctl 설치 완료 ~!~!~!~!~!~!'
 
 # kubectl 설치
 wget -P $HOME/cli/  https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
@@ -15,12 +16,13 @@ echo 'source <(kubectl completion bash)' >>$HOME/.bashrc
 echo 'alias k=kubectl' >>$HOME/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>$HOME/.bashrc
 source $HOME/.bashrc
+echo '~!~!~!~!~!~! kubectl 설치 완료 ~!~!~!~!~!~!'
 
 # terraform 설치
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 terraform -install-autocomplete
-echo terraform 설치 완료 했습니다. 명령어 자동 완성 하려면 ssh 재 접속하세요^_^
+echo '~!~!~!~!~!~! terraform 설치 완료 ~!~!~!~!~!~!'
 
 # docker 설치
 sudo yum install -y docker
@@ -30,12 +32,13 @@ sudo usermod -a -G docker ec2-user
 wget -P $HOME/cli/ https://raw.githubusercontent.com/docker/cli/refs/heads/master/contrib/completion/bash/docker
 sudo cp $HOME/cli/docker /etc/bash_completion.d/docker
 source /etc/bash_completion.d/docker
-echo ec2 server 재접속 하세요
+echo '~!~!~!~!~!~! docker 설치 완료 ~!~!~!~!~!~!'
 
 # helm 설치
 wget -O $HOME/cli/get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 $HOME/cli/get_helm.sh
 $HOME/cli/get_helm.sh
 echo 'source <(helm completion bash)' >> ~/.bashrc
-echo 모든 CLI가 설치 됐습니다.
-echo 서버를 재 접속 하세요~
+echo '~!~!~!~!~!~! helm 설치 완료 ~!~!~!~!~!~!'
+echo 'eksctl, kubectl, terraform, docker, helm CLI 설치 완료 됐어요.'
+echo 'EC2 SSH 재 접속 하세요'
